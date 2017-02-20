ba2a93e2c4d9dbf35b67edde934ff19edacdb107
Ibe Vanmeenen <ibevanmeenen@hotmail.com>
2017-02-20 21:49:09 +0100

    Start

---
+++ app/js/components/_component.js
@@ -0,0 +1,26 @@
+/* ==========================================================================
+   Webstarter - Component
+   ========================================================================== */
+
+webstarter.component = () => {
+
+    let exports = webstarter.component;
+
+
+    // Private function
+    const privateFunction = () => {
+
+    };
+
+
+    // Public function
+    exports.publicFunction = () => {
+        // Public function
+    };
+
+
+    // Init
+    const init = (() => {
+        // Init (self-executing function)
+    })();
+};
